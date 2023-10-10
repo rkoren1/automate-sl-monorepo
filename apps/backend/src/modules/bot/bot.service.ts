c309c78de4a03bd29fa75b4a1a54e21dcb0692c6
Rok Koren <rok.koren@netica.si>
2023-10-10 17:47:34 +0200

    fixed refreshbotstatus api

--- apps/backend/src/modules/bot/bot.service.ts
+++ apps/backend/src/modules/bot/bot.service.ts
@@ -324,7 +324,7 @@ export class BotService {
       return true;
     }
     //else check if bot is offline and set running to false
-    if (this.botInstances[botId]?.isConnected) {
+    if (!this.botInstances[botId]?.isConnected) {
       bot.running = false;
       await this.em.persistAndFlush(bot);
       return true;

09e9c96b8e9af35490baf401ee995ef2540f1c7d
rkoren1 <rok.koren@netica.si>
2023-12-03 16:49:57 +0100

    updated ng matero

--- apps/frontend/src/app/material-extensions.module.ts
+++ apps/frontend/src/app/material-extensions.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-
 import { MtxAlertModule } from '@ng-matero/extensions/alert';
 import { MtxButtonModule } from '@ng-matero/extensions/button';
 import { MtxCheckboxGroupModule } from '@ng-matero/extensions/checkbox-group';
@@ -12,11 +11,11 @@ import { MtxLoaderModule } from '@ng-matero/extensions/loader';
 import { MtxPopoverModule } from '@ng-matero/extensions/popover';
 import { MtxProgressModule } from '@ng-matero/extensions/progress';
 import { MtxSelectModule } from '@ng-matero/extensions/select';
-import { MtxSliderModule } from '@ng-matero/extensions/slider';
 import { MtxSplitModule } from '@ng-matero/extensions/split';
 import { MtxTooltipModule } from '@ng-matero/extensions/tooltip';
 import { MTX_DATETIME_FORMATS } from '@ng-matero/extensions/core';
 import { MtxMomentDatetimeModule } from '@ng-matero/extensions-moment-adapter';
+import { MatSliderModule } from '@angular/material/slider';
 
 @NgModule({
   exports: [
@@ -32,10 +31,10 @@ import { MtxMomentDatetimeModule } from '@ng-matero/extensions-moment-adapter';
     MtxPopoverModule,
     MtxProgressModule,
     MtxSelectModule,
-    MtxSliderModule,
+    MatSliderModule,
     MtxSplitModule,
     MtxTooltipModule,
-    MtxMomentDatetimeModule, // <= You can import the other adapter you need (luxon, date-fns)
+    MtxMomentDatetimeModule,
   ],
   providers: [
     {

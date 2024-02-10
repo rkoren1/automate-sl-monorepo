42c0afb5346c2d2e34aff9767716b3d4ad5f6a91
rkoren1 <rok.koren@netica.si>
2024-02-10 17:33:01 +0100

    added pascalCase migration

---
+++ apps/backend/prisma/migrations/20240210162835_pascal_case_rename/migration.sql
@@ -0,0 +1,55 @@
+-- Rename to PascalCase
+ALTER TABLE `bot`
+RENAME TO  `Bot`;
+
+-- Rename to PascalCase
+ALTER TABLE `botLog`
+RENAME TO  `BotLog`;
+
+-- Rename to PascalCase
+ALTER TABLE `groupImAvatar`
+RENAME TO  `GroupImAvatar`;
+
+-- Rename to PascalCase
+ALTER TABLE `groupImList`
+RENAME TO  `GroupImList`;
+
+-- Rename to PascalCase
+ALTER TABLE `package`
+RENAME TO  `Package`;
+
+-- Rename to PascalCase
+ALTER TABLE `paymentLog`
+RENAME TO  `PaymentLog`;
+
+-- Rename to PascalCase
+ALTER TABLE `sharedBotUserSubscription`
+RENAME TO  `SharedBotUserSubscription`;
+
+-- Rename to PascalCase
+ALTER TABLE `subscription`
+RENAME TO  `Subscription`;
+
+-- Rename to PascalCase
+ALTER TABLE `coupon`
+RENAME TO  `Coupon`;
+
+-- Rename to PascalCase
+ALTER TABLE `discordSettings`
+RENAME TO  `DiscordSettings`;
+
+-- Rename to PascalCase
+ALTER TABLE `sharedBot`
+RENAME TO  `SharedBot`;
+
+-- Rename to PascalCase
+ALTER TABLE `terminal`
+RENAME TO  `Terminal`;
+
+-- Rename to PascalCase
+ALTER TABLE `terminalOwner`
+RENAME TO  `TerminalOwner`;
+
+-- Rename to PascalCase
+ALTER TABLE `user`
+RENAME TO  `User`;

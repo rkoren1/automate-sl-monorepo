3da1a38329aa3a186c3d03f389d12d365e423237
Rok Koren <rok.koren@netica.si>
2024-02-11 19:13:41 +0100

    fixed debugger

--- apps/backend/webpack.config.js
+++ apps/backend/webpack.config.js
@@ -1,8 +1,18 @@
-const { composePlugins, withNx} = require('@nx/webpack');
+const { composePlugins, withNx, NxWebpackPlugin } = require('@nx/webpack');
 
 // Nx plugins for webpack.
 module.exports = composePlugins(withNx(), (config) => {
   // Update the webpack config as needed here.
   // e.g. `config.plugins.push(new MyPlugin())`
+  config.plugins.push(
+    new NxWebpackPlugin({
+      sourceMap: true,
+      main: 'apps/backend/src/main.ts',
+      target: 'node',
+      compiler: 'tsc',
+      tsConfig: 'apps/backend/tsconfig.app.json',
+      outputPath: 'dist/apps/backend',
+    }),
+  );
   return config;
 });

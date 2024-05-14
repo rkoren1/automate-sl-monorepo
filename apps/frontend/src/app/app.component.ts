cd4375755fb52def4385eed1c3337df986b8797d
rkoren1 <rok.koren@netica.si>
2024-05-14 13:11:13 +0200

    added google analytics

--- apps/frontend/src/app/app.component.ts
+++ apps/frontend/src/app/app.component.ts
@@ -1,6 +1,11 @@
 import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { Observable, filter } from 'rxjs';
 import { PreloaderService } from './core/bootstrap/preloader.service';
 import { SettingsService } from './core/bootstrap/settings.service';
+
+// eslint-disable-next-line no-var, @typescript-eslint/no-explicit-any
+declare var gtag: any;
 
 @Component({
   selector: 'app-root',
@@ -9,8 +14,16 @@ import { SettingsService } from './core/bootstrap/settings.service';
 export class AppComponent implements OnInit, AfterViewInit {
   constructor(
     private preloader: PreloaderService,
-    private settings: SettingsService
-  ) {}
+    private settings: SettingsService,
+    router: Router,
+  ) {
+    const navEndEvent$: Observable<NavigationEnd> = router.events.pipe(
+      filter((e): e is NavigationEnd => e instanceof NavigationEnd),
+    );
+    navEndEvent$.subscribe((e: NavigationEnd) => {
+      gtag('config', 'G-QPGSZQQ76X', { page_path: e.urlAfterRedirects });
+    });
+  }
 
   ngOnInit() {
     this.settings.setDirection();
